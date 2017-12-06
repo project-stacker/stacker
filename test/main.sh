7da7a442c87a03bfe6f0676c245699ce15b813ca
Tycho Andersen <tycho@tycho.ws>
2017-12-05 17:05:25 -0700

    add STACKER_KEEP and STACKER_INSPECT to test stuite
    STACKER_KEEP keeps stuff in your .stacker directory for the test suite so
    you don't have to redownload everything.
    
    STACKER_INSPECT lets you inspect the result before teardown on failure.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- test/main.sh
+++ test/main.sh
@@ -18,14 +18,28 @@ function sha() {
 
 function cleanup() {
     set +x
-    umount roots >& /dev/null || true
-    rm -rf roots .stacker >& /dev/null || true
     if [ "$RESULT" != "success" ]; then
+        if [ -n "$STACKER_INSPECT" ]; then
+            echo "waiting for inspection; press enter to continue cleanup"
+            read -r foo
+        fi
         RESULT=failure
+    fi
+    umount roots >& /dev/null || true
+    rm -rf roots >& /dev/null || true
+    if [ -z "$STACKER_KEEP" ]; then
+        rm -rf .stacker >& /dev/null || true
+    else
+        rm -rf .stacker/logs .stacker/btrfs.loop
     fi
     echo done with testing: $RESULT
 }
 trap cleanup EXIT HUP INT TERM
+
+# clean up old logs if they exist
+if [ -n "$STACKER_KEEP" ]; then
+    rm -rf .stacker/logs .stacker/btrfs.loop
+fi
 
 set -x
 
