aa88921fc94ede381859eefc5293da403e17c6f3
Tycho Andersen <tycho@tycho.ws>
2018-01-03 00:37:49 +0000

    use pb to estimate import times
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- network.go
+++ network.go
@@ -6,10 +6,26 @@ import (
 	"net/http"
 	"os"
 	"path"
+
+	"github.com/cheggaaa/pb"
 )
 
 // download with caching support in the specified cache dir.
 func download(cacheDir string, url string) (string, error) {
+	name := path.Join(cacheDir, path.Base(url))
+	out, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		// It already exists, let's just use that one.
+		if os.IsExist(err) {
+			fmt.Println("using cached copy of", url)
+			return name, nil
+		}
+		return "", err
+	}
+	defer out.Close()
+
+	fmt.Println("downloading", url)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", err
@@ -20,20 +36,16 @@ func download(cacheDir string, url string) (string, error) {
 		return "", fmt.Errorf("couldn't download %s: %s", url, resp.Status)
 	}
 
-	if err := os.MkdirAll(cacheDir, 0755); err != nil {
-		return "", err
+	source := resp.Body
+	if resp.ContentLength >= 0 {
+		bar := pb.New(int(resp.ContentLength)).SetUnits(pb.U_BYTES)
+		bar.ShowTimeLeft = true
+		bar.ShowSpeed = true
+		bar.Start()
+		source = bar.NewProxyReader(source)
+		defer bar.Finish()
 	}
 
-	name := path.Join(cacheDir, path.Base(resp.Request.URL.Path))
-	out, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0644)
-	if err != nil {
-		// It already exists, let's just use that one.
-		if os.IsExist(err) {
-			return name, nil
-		}
-		return "", err
-	}
-
-	_, err = io.Copy(out, resp.Body)
+	_, err = io.Copy(out, source)
 	return name, err
 }
