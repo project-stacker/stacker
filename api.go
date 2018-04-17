project-stacker/stacker

commit 4374239ccae38c799a52d466694015374fc0217d

oci import: don't use docker tag in oci tag
This is confusing for people trying to manually maniuplate .stacker out of
band, and generally unnecessary the way we're using it, so let's drop it.

Signed-off-by: Tycho Andersen <tycho@tycho.ws>

api.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/url"
-	"path"
 	"reflect"
 	"strings"
 
@@ -69,14 +68,18 @@ func (is *ImageSource) ParseTag() (string, error) {
 		}
 
 		if url.Path != "" {
-			tag := path.Base(strings.Replace(url.Path, ":", "-", -1))
-			return tag, nil
+			return strings.Split(url.Path, ":")[0], nil
 		}
 
 		// skopeo allows docker://centos:latest or
 		// docker://docker.io/centos:latest; if we don't have a
 		// url path, let's use the host as the image tag
-		return strings.Replace(url.Host, ":", "-", -1), nil
+		return strings.Split(url.Host, ":")[0], nil
+	case OCIType:
+		pieces := strings.Split(is.Url, ":")
+		if len(pieces) != 2 {
+			return "", fmt.Errorf("bad OCI tag: %s", is.Type)
+		}
 
 	default:
 		return "", fmt.Errorf("unsupported type: %s", is.Type)

commit 5c8b086e7c9b61552902c53900ae241b3fcad618

fix docker path
commit 4374239cca broke some builds by trying to skopeo copy
images to, say, "oci:oci:anuvu/anuvu-dev".  That's because
it dropped the 'path.Base(anuvu/anuvu-dev)'.  Re-add that.

Signed-off-by: Serge Hallyn <shallyn@cisco.com>

api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/url"
+	"path"
 	"reflect"
 	"strings"
 
@@ -68,7 +69,7 @@ func (is *ImageSource) ParseTag() (string, error) {
 		}
 
 		if url.Path != "" {
-			return strings.Split(url.Path, ":")[0], nil
+			return path.Base(strings.Split(url.Path, ":")[0]), nil
 		}
 
 		// skopeo allows docker://centos:latest or