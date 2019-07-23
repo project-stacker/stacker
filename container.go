704285ca11e7da8c2cdef7c5b17ee2d05d222db7
ovalea <ovalea@cisco.com>
2019-07-23 03:38:30 -0700

    Enabled setting proxy for FTP

--- container.go
+++ container.go
@@ -146,7 +146,7 @@ func newContainer(sc StackerConfig, name string) (*container, error) {
 		return nil, err
 	}
 
-	for _, k := range []string{"http_proxy", "https_proxy", "no_proxy", "TERM"} {
+	for _, k := range []string{"http_proxy", "https_proxy", "no_proxy", "ftp_proxy", "TERM"} {
 		v := os.Getenv(k)
 		if v != "" {
 			err = c.setConfig("lxc.environment", fmt.Sprintf("%s=%s", k, v))
