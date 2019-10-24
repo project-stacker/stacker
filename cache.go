982c0032ce1a2cebf45d31904d920f20cabaa549
Tycho Andersen <tycho@tycho.ws>
2019-10-24 14:14:18 -0600

    import: consider URL change
    When computing whether or not two *layers* were the same, we correctly took
    the URL into account and forced a rebuild. However, when computing whether
    two *imports* were the same and needed to be re-downloaded, we did not.
    Instead, we just looked to see if a file with that particular basename was
    present or not.
    
    This problem can also occur with local paths (i.e. not just URLs).
    
    So, let's look at the old cache entry and see if any of the import URLs for
    a particular directive have changed and if they have, just delete that from
    the cache before we do imports.
    
    Perhaps this should be a part of the caching code itself, that when there
    is an explicit cache miss because of layer import changes, it should delete
    the stuff from the cache. It also seems weird to have a cache that mutates
    the disk, though. So I'll leave it like this for now.
    
    Fixes #55 take two :)
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- cache.go
+++ cache.go
@@ -20,7 +20,7 @@ import (
 	"github.com/vbatts/go-mtree"
 )
 
-const currentCacheVersion = 5
+const currentCacheVersion = 6
 
 type ImportType int
 
@@ -169,6 +169,11 @@ func hashFile(path string) (string, error) {
 	return d.String(), nil
 }
 
+func (c *BuildCache) LookupWithoutChecks(name string) (CacheEntry, bool) {
+	e, ok := c.Cache[name]
+	return e, ok
+}
+
 func (c *BuildCache) Lookup(name string) (*CacheEntry, bool) {
 	l, ok := c.sfm.LookupLayerDefinition(name)
 	if !ok {
@@ -209,12 +214,12 @@ func (c *BuildCache) Lookup(name string) (*CacheEntry, bool) {
 	}
 
 	for _, imp := range imports {
-		fname := path.Base(imp)
-		cachedImport, ok := result.Imports[fname]
+		cachedImport, ok := result.Imports[imp]
 		if !ok {
 			return nil, false
 		}
 
+		fname := path.Base(imp)
 		diskPath := path.Join(c.importsDir, name, fname)
 		st, err := os.Stat(diskPath)
 		if err != nil {
@@ -289,7 +294,7 @@ func (c *BuildCache) getBaseHash(name string) (string, error) {
 		return "", nil
 	}
 
-	baseEnt, ok := c.Lookup(l.From.Tag)
+	baseEnt, ok := c.LookupWithoutChecks(l.From.Tag)
 	if !ok {
 		return "", fmt.Errorf("couldn't find a cache of base layer")
 	}
@@ -349,7 +354,7 @@ func (c *BuildCache) Put(name string, blob ispec.Descriptor) error {
 			}
 		}
 
-		ent.Imports[fname] = ih
+		ent.Imports[imp] = ih
 	}
 
 	c.Cache[name] = ent
