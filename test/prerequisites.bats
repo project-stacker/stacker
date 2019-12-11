project-stacker/stacker

commit dce7c10c86b4c67f5cd54ce2c7e8da43227420c1

base: fix incorrect importing of base layers
The goal of the import here is to get whatever OCI stuff that already
exsits into the output image, so that we can add the new stuff on top of
this.

We were doing this incorrectly: we don't want to go to the first non-built
layer, in fact we want the closest reasonable layer to what's there now. So
instead of doing a recursive look through the layer definitions, let's look
at the base layer's output, if it doesn't exist, look for its input, and if
that doesn't exist, give up and put the whole rootfs in the next OCI
output.

Signed-off-by: Tycho Andersen <tycho@tycho.ws>

base.go
@@ -348,50 +348,47 @@ func getBuilt(o BaseLayerOpts, sfm StackerFiles) error {
 	// may not have been copied before and the final `umoci repack` expects
 	// them to be there.
 	targetName := o.Name
-	base := o.Layer
-	for {
-		// Iterate through base layers until we find the first one which is not BuiltType
-		// Need to declare ok separately, if we do it in the same line as
-		// assigning the new value to base, base would be a new variable only in the scope
-		// of this iteration and we never meet the condition to exit the loop
-		var ok bool
-		base, ok = sfm.LookupLayerDefinition(base.From.Tag)
-		if !ok {
-			return fmt.Errorf("missing base layer: %s?", base.From.Tag)
+	baseLayerDefinition, ok := sfm.LookupLayerDefinition(o.Layer.From.Tag)
+	if !ok {
+		return errors.Errorf("missing base layer: %s?", o.Layer.From.Tag)
+	}
+
+	if baseLayerDefinition.BuildOnly {
+		baseInputTag, err := baseLayerDefinition.From.ParseTag()
+		if err != nil {
+			return err
 		}
 
-		if base.From.Type != BuiltType {
-			break
+		// Ok, we didn't generate an OCI diff for whatever mutated this
+		// layer. So either 1. the layer was sourced
+		// from something and is in the OCI cache, so we can copy at
+		// least the base's base over to make the delta smaller or 2.
+		// it was sourced from e.g. a tar file, in which case there's
+		// nothing we can do besides initialize an empty oci tag and
+		// generate the whole thing.
+		cacheDir := path.Join(o.Config.StackerDir, "layer-bases", "oci")
+		err = lib.ImageCopy(lib.ImageCopyOpts{
+			Src:  fmt.Sprintf("oci:%s:%s", cacheDir, baseInputTag),
+			Dest: fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, targetName),
+		})
+		if err != nil {
+			// give up and just create an empty image
+			return umociInit(o)
 		}
-	}
-
-	// Nothing to do here -- we didn't import any base layers.
-	if (base.From.Type != DockerType && base.From.Type != OCIType && base.From.Type != ZotType) || !base.BuildOnly {
 		return nil
-	}
+	} else {
+		baseOutputTag, err := o.Layer.From.ParseTag()
+		if err != nil {
+			return err
+		}
 
-	// Nothing to do here either -- the previous step emitted a layer with
-	// the base's tag name. We don't want to overwrite that with a stock
-	// base layer.
-	if !base.BuildOnly {
-		return nil
+		refs, _ := o.OCI.ListReferences(context.Background())
+		fmt.Printf("refs: %v\n", refs)
+		return lib.ImageCopy(lib.ImageCopyOpts{
+			Src:  fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, baseOutputTag),
+			Dest: fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, targetName),
+		})
 	}
-
-	tag, err := base.From.ParseTag()
-	if err != nil {
-		return err
-	}
-
-	cacheDir := path.Join(o.Config.StackerDir, "layer-bases", "oci")
-	err = lib.ImageCopy(lib.ImageCopyOpts{
-		Src:  fmt.Sprintf("oci:%s:%s", cacheDir, tag),
-		Dest: fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, targetName),
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func ComputeAggregateHash(manifest ispec.Manifest, descriptor ispec.Descriptor) (string, error) {

commit dbd2367e21ec0faae156f41287d900366e347356

In case of build-only layers, look for base layers in OCIDir before looking into layer-bases
Fix bug introduced in: https://github.com/anuvu/stacker/commit/dce7c10c86b4c67f5cd54ce2c7e8da43227420c1
If a build-only layer depended on a non-build-only layer which was previously built (not downloaded), it would try to copy the base image from 'layer-bases' instead of the OCIDir where stacker places it

base.go
@@ -366,6 +366,17 @@ func getBuilt(o BaseLayerOpts, sfm StackerFiles) error {
 		// it was sourced from e.g. a tar file, in which case there's
 		// nothing we can do besides initialize an empty oci tag and
 		// generate the whole thing.
+
+		// Attempt to copy the base from o.Config.OCIDir layout in case it's already there
+		err = lib.ImageCopy(lib.ImageCopyOpts{
+			Src:  fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, baseInputTag),
+			Dest: fmt.Sprintf("oci:%s:%s", o.Config.OCIDir, targetName),
+		})
+		if err == nil {
+			return nil
+		}
+
+		// Attempt to copy the base from the layer-bases layout
 		cacheDir := path.Join(o.Config.StackerDir, "layer-bases", "oci")
 		err = lib.ImageCopy(lib.ImageCopyOpts{
 			Src:  fmt.Sprintf("oci:%s:%s", cacheDir, baseInputTag),

test/prerequisites.bats
@@ -126,3 +126,32 @@ function teardown() {
     [ -f dest/layer3_2/rootfs/root/import0_copied ]
     [ -f dest/layer3_2/rootfs/root/import0 ]
 }
+
+@test "build layers and prerequisites containing build-only layer" {
+    mkdir -p ocibuilds/sub4
+    cat > ocibuilds/sub4/stacker.yaml <<EOF
+config:
+    prerequisites:
+        - ../sub1/stacker.yaml
+layer4_1:
+    from:
+        type: built
+        tag: layer1_2
+    run: |
+        touch /root/import4
+    build_only: true
+layer4_2:
+    from:
+        type: built
+        tag: layer4_1
+    run: |
+        cp /root/import4 /root/import4_copied
+EOF
+    stacker build -f ocibuilds/sub4/stacker.yaml
+    mkdir dest
+    umoci unpack --image oci:layer4_2 dest/layer4_2
+    [ "$status" -eq 0 ]
+    [ -f dest/layer4_2/rootfs/root/import4_copied ]
+    [ -f dest/layer4_2/rootfs/root/import4 ]
+    [ -f dest/layer4_2/rootfs/root/import0 ]
+}