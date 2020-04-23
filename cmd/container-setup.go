15acdf57da9d30602c0195a96b2dbefa44dd7b78
Tycho Andersen <tycho@tycho.ws>
2020-04-23 16:24:48 -0600

    container-setup: add a note about the ro-ness of snapshots
    also, hide container-setup by default, since it is sort of an abuse of the
    stacker API.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- cmd/container-setup.go
+++ cmd/container-setup.go
@@ -8,9 +8,28 @@ import (
 var containerSetupCmd = cli.Command{
 	Name:   "container-setup",
 	Usage:  "set up (but don't run) any containers in the stacker file",
+	Hidden: true,
 	Action: doContainerSetup,
 	Flags:  initBuildFlags(),
 	Before: beforeBuild,
+	ArgsUsage: `
+
+container-setup allows you to use stacker's OCI image handling and LXC config
+generation code without actually doing a build. However, it does circumvent a
+lot of the underlying cache functionality of stacker; you have been warned.
+
+Additionally, stacker leaves its btrfs snapshots in the default (i.e.
+read-only) state. If there are bind mountpoints that need to be created (or
+other things liblxc or your subsequent code that runs in the container needs to
+write to the filesystem), you'll want to mark them writable with:
+
+btrfs property set -ts "$path_to_snapshot" ro false
+
+Again, though, this circumvents stacker's awareness of its own cache, and which
+may potentially screw things up.
+
+We reserve the right to change this behavior at any time :)
+`,
 }
 
 func doContainerSetup(ctx *cli.Context) error {
