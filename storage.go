e1aeb02168d8aa230c951eb20f2651a48e567f35
Tycho Andersen <tycho@tycho.ws>
2020-06-17 15:59:53 -0600

    storage: get rid of duplicate isMounted
    We have IsMountpoint(), which is a better name. Let's just use that.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- storage.go
+++ storage.go
@@ -408,7 +408,7 @@ func (b *btrfs) Clean() error {
 // MakeLoopbackBtrfs creates a btrfs filesystem mounted at dest out of a loop
 // device and allows the specified uid to delete subvolumes on it.
 func MakeLoopbackBtrfs(loopback string, size int64, uid int, gid int, dest string) error {
-	mounted, err := isMounted(loopback)
+	mounted, err := IsMountpoint(loopback)
 	if err != nil {
 		return err
 	}
@@ -494,22 +494,4 @@ func setupLoopback(path string, uid int, gid int, size int64) error {
 	}
 
 	return nil
-}
-
-func isMounted(path string) (bool, error) {
-	f, err := os.Open("/proc/self/mountinfo")
-	if err != nil {
-		return false, err
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, path) {
-			return true, nil
-		}
-	}
-
-	return false, nil
 }
