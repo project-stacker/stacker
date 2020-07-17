845287c29659cc0ddfb6b024c98dc54be38a521f
Tycho Andersen <tycho@tycho.ws>
2020-07-17 08:25:49 -0600

    drop btrfs layer type
    we haven't used this in a long time, so let's drop it.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.ws>

--- api.go
+++ api.go
@@ -1,7 +1,6 @@
 package stacker
 
 const (
-	MediaTypeImageBtrfsLayer  = "application/vnd.cisco.image.layer.btrfs"
 	GitVersionAnnotation      = "com.cisco.stacker.git_version"
 	StackerContentsAnnotation = "com.cisco.stacker.stacker_yaml"
 )
