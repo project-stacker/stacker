9289fb2f35e87e99681273719787ede25a39e5ca
Tycho Andersen <tycho@tycho.pizza>
2020-12-04 06:09:50 -0800

    overlay: fix linting error
    Derp :(
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- overlay/unpriv-setup.go
+++ overlay/unpriv-setup.go
@@ -1,8 +1,8 @@
 package overlay
 
 import (
-	"github.com/anuvu/stacker/types"
 	"github.com/anuvu/stacker/container"
+	"github.com/anuvu/stacker/types"
 )
 
 func UnprivSetup(config types.StackerConfig, uid, gid int) error {
