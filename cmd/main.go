2d266ece01200b871e917ce5c132d821bec4b0ef
Tycho Andersen <tycho@tycho.pizza>
2021-10-19 17:26:58 -0600

    unpriv-setup: really don't run in a userns
    4f112bfa303e ("unpriv-setup: don't run inside a userns") made a typo in the
    test... let's fix that.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- cmd/main.go
+++ cmd/main.go
@@ -258,7 +258,7 @@ func main() {
 		stackerlog.FilterNonStackerLogs(handler, logLevel)
 		stackerlog.Debugf("stacker version %s", version)
 
-		if !ctx.Bool("internal-userns") && len(ctx.Args()) >= 1 && ctx.Args()[0] != "unpriv-stacker" {
+		if !ctx.Bool("internal-userns") && len(ctx.Args()) >= 1 && ctx.Args()[0] != "unpriv-setup" {
 			binary, err := os.Readlink("/proc/self/exe")
 			if err != nil {
 				return err
