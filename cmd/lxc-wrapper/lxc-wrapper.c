8ab66dd92fbc987c6260f32594b5a40a7268c4dc
Scott Moser <scmoser@cisco.com>
2022-11-04 14:34:13 -0700

    feat: Update lxc-wrapper to support 'usernsexec' and 'nsexec'
    The functionality added here is to add 'usernsexec' and 'nsexec'
    subcommands to lxc-wrapper.  The 'usernsexec' performs as it
    did before.  'nsexec' should perform the same as usernsexec
    but not use a user namespace.
    
    The code changes are mostly un-thoughtfully taken from an example
    in user_namespaces(7).
    
    usernsexec will put you into user (CLONE_NEWUSER) namespace.
    Both usernsexec and nsexec will put the command into its own
    private mount (MS_PRIVATE and CLONE_NEWNS) and pid (CLONE_NEWPID)
    namespace.
    
    Using mount and pid namespaces allow for being "sloppy" in leaving
    subprocesses or mounts open.  The kernel will clean them all up.
    
    Note, the reason for 'nsexec' is mainly to support 'sudo' builds.
    If 'sudo stacker' immediately jumped into a root namespace, then
    it ends up being unable to write to many of the user-owned (SUDO_UID)
    files or directories.  Using nsexec, root retains all its permissions
    but still puts itself into a pid and mount namespace.
    
    Generally speaking, when someone types 'sudo <something>', they
    expect that that something will have *elevated* privledges.  Having
    stacker immediately drop into root's user namespace meant it really
    had no additional privs.
    
    Signed-off-by: Scott Moser <scmoser@cisco.com>

--- cmd/lxc-wrapper/lxc-wrapper.c
+++ cmd/lxc-wrapper/lxc-wrapper.c
@@ -6,11 +6,20 @@
 #include <signal.h>
 #include <sched.h>
 #include <errno.h>
+#include <sys/mount.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/socket.h>
 
 #include <lxc/lxccontainer.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+struct child_args {
+	char **argv;          /* Command to be executed by child, with args */
+	int    sk_pair[2];    /* Socket used to synchronize parent and child */
+	int    command_start;
+};
 
 static int spawn_container(char *name, char *lxcpath, char *config)
 {
@@ -71,102 +80,153 @@ static int map_id(char *args[])
 	return 0;
 }
 
-static int do_usernsexec(int argc, char *argv[], int *status)
+static int childFunc (void *arg) {
+	struct child_args *args = arg;
+	char c = 'x';
+	int ret = 0;
+
+	close(args->sk_pair[0]);
+
+	// The intent is that our child process is in:
+	//  * user namespace for 'usernsexec' (CLONE_NEWUSER)
+	//  * private (MS_PRIVATE) mount namespace always (CLONE_NEWNS)
+	//  * pid namespace always (CLONE_NEWPID)
+	//
+	// If not in its' own user namespace, then mounts will propagate.
+	// change that behavior so it is consistent for both cases.
+	ret = mount("none", "/", 0, MS_PRIVATE | MS_REC, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "entering private mount namespace failed: %m\n");
+		close(args->sk_pair[1]);
+		exit(1);
+	}
+
+	ret = mount("proc", "/proc", "proc", 0, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "mounting proc failed: %m\n");
+		close(args->sk_pair[1]);
+		exit(1);
+	}
+
+	// tell the parent we are ready.
+	if (write(args->sk_pair[1], &c, 1) != 1) {
+		fprintf(stderr, "write: %m\n");
+		close(args->sk_pair[1]);
+		exit(1);
+	}
+
+	// wait for the parent to map our ids
+	if (read(args->sk_pair[1], &c, 1) != 1) {
+		fprintf(stderr, "child read(): %m\n");
+		close(args->sk_pair[1]);
+		exit(1);
+	}
+
+	close(args->sk_pair[1]);
+
+	// let's party
+	execvp(args->argv[args->command_start], args->argv + args->command_start);
+	fprintf(stderr, "usernsexec failed\n");
+	exit(1);
+}
+
+
+static int do_nsexec(char* mode, int argc, char *argv[], int *status)
 {
 	pid_t pid;
-	int sk_pair[2], ret, cur, group_start = -1, command_start = -1;
+	int ret, cur, group_start = -1, command_start = -1;
 	char c = 'x', thepid[20];
+	static char child_stack[STACK_SIZE];
+	struct child_args args;
 
-	for (cur = 1; argv[cur] != NULL; cur++) {
-		if (!strcmp(argv[cur], "g")) {
-			group_start = cur;
-		}
+	// userns - should a userns be used?
+	int userns = 0;
+	int flags = 0;
+	flags |= CLONE_NEWNS;
+	flags |= CLONE_NEWPID;
 
-		if (!strcmp(argv[cur], "--")) {
-			command_start = cur;
+	if (!strcmp(mode, "usernsexec")) {
+		userns = 1;
+		flags |= CLONE_NEWUSER;
+		// usernsexec args are:
+		//   u nsUidStart hostUidStart range [...]
+		//	 [g [nsGidStart hostGidStart range ]...]
+		//	 -- command args
+		for (cur = 1; argv[cur] != NULL; cur++) {
+			if (!strcmp(argv[cur], "g")) {
+				group_start = cur;
+			}
+
+			if (!strcmp(argv[cur], "--")) {
+				command_start = cur;
+			}
 		}
+	} else if (!strcmp(mode, "nsexec")) {
+		for (cur = 1; argv[cur] != NULL; cur++) {
+			if (!strcmp(argv[cur], "--")) {
+				command_start = cur;
+			}
+		}
+	} else {
+		fprintf(stderr, "Invalid mode '%s'\n", mode);
+		return -1;
 	}
 
 	if (command_start < 0) {
-		fprintf(stderr, "no command to usernsexec found\n");
+		fprintf(stderr, "no command to %s found ('--' is required)\n", mode);
 		goto out;
 	}
 
-
-	if (socketpair(PF_LOCAL, SOCK_SEQPACKET, 0, sk_pair) < 0) {
+	if (socketpair(PF_LOCAL, SOCK_SEQPACKET, 0, args.sk_pair) < 0) {
 		fprintf(stderr, "socketpair(): %m\n");
 		return -1;
 	}
 
-	pid = fork();
+	args.argv = &argv[optind];
+	args.command_start = command_start;
+
+	pid = clone(childFunc, child_stack + STACK_SIZE, flags | SIGCHLD, &args);
 	if (pid < 0) {
-		close(sk_pair[0]);
-		close(sk_pair[1]);
+		close(args.sk_pair[0]);
+		close(args.sk_pair[1]);
 		fprintf(stderr, "fork(): %m\n");
 		return -1;
 	}
 
-	if (!pid) {
-		close(sk_pair[0]);
-
-		if (unshare(CLONE_NEWUSER | CLONE_NEWNS)) {
-			fprintf(stderr, "unshare: %m\n");
-			close(sk_pair[1]);
-			exit(1);
-		}
-
-		// tell the parent we unshared
-		if (write(sk_pair[1], &c, 1) != 1) {
-			fprintf(stderr, "write: %m\n");
-			close(sk_pair[1]);
-			exit(1);
-		}
-
-		// wait for the parent to map our ids
-		if (read(sk_pair[1], &c, 1) != 1) {
-			fprintf(stderr, "child read(): %m\n");
-			close(sk_pair[1]);
-			exit(1);
-		}
-		close(sk_pair[1]);
-
-		// let's party
-		execvp(argv[command_start+1], argv + command_start + 1);
-		fprintf(stderr, "usernsexec failed\n");
-		exit(1);
-	}
-
-	close(sk_pair[1]);
+	close(args.sk_pair[1]);
 	ret = -1;
 
 	// wait for child to unshare
-	if (read(sk_pair[0], &c, 1) != 1) {
+	if (read(args.sk_pair[0], &c, 1) != 1) {
 		fprintf(stderr, "parent read(): %m\n");
 		goto out;
 	}
 	sprintf(thepid, "%d", pid);
 
-	// pretty brutal hack: we just re-use our argv array for exec-ing
-	// new{u,g}idmap, placing NULLs and a few names in the right spots.
-	// we assume first argument is uid maps and that at least one is always
-	// present for root
-	argv[0] = "newuidmap";
-	argv[1] = thepid;
-	argv[group_start] = NULL;
-	if (map_id(argv))
-		goto out;
+	if (userns) {
+		// pretty brutal hack: we just re-use our argv array for exec-ing
+		// new{u,g}idmap, placing NULLs and a few names in the right spots.
+		// we assume first argument is uid maps and that at least one is always
+		// present for root
+		argv[0] = "newuidmap";
+		argv[1] = thepid;
+		argv[group_start] = NULL;
+		if (map_id(argv))
+			goto out;
 
-	// if there was a group mapping, set it too
-	if (group_start > 0) {
-		argv[group_start-1] = "newgidmap";
-		argv[group_start] = thepid;
-		argv[command_start] = NULL;
+		// if there was a group mapping, set it too
+		if (group_start > 0) {
+			argv[group_start-1] = "newgidmap";
+			argv[group_start] = thepid;
+			argv[command_start] = NULL;
+		}
+
 		if (map_id(argv + group_start-1))
 			goto out;
 	}
 
 	// tell child it's ok to exec
-	if (write(sk_pair[0], &c, 1) != 1) {
+	if (write(args.sk_pair[0], &c, 1) != 1) {
 		fprintf(stderr, "write(): %m\n");
 		goto out;
 	}
@@ -179,7 +239,7 @@ static int do_usernsexec(int argc, char *argv[], int *status)
 	ret = 0;
 
 out:
-	close(sk_pair[0]);
+	close(args.sk_pair[0]);
 	return ret;
 }
 
@@ -192,8 +252,6 @@ int main(int argc, char *argv[])
 
 	if (!strcmp(argv[1], "spawn")) {
 		int ret, status;
-		char buf[4096];
-		ssize_t size;
 		char *name, *lxcpath, *config_path;
 
 		if (argc != 5) {
@@ -226,10 +284,10 @@ int main(int argc, char *argv[])
 			kill(0, WTERMSIG(status));
 			exit(EXIT_FAILURE);
 		}
-	} else if (!strcmp(argv[1], "usernsexec")) {
+	} else if (!strcmp(argv[1], "usernsexec") || !strcmp(argv[1], "nsexec")) {
 		int ret, status;
 
-		ret = do_usernsexec(argc-1, argv+1, &status);
+		ret = do_nsexec(argv[1], argc-1, argv+1, &status);
 		if (ret)
 			exit(EXIT_FAILURE);
 		if (WIFSIGNALED(status)) {
@@ -242,7 +300,7 @@ int main(int argc, char *argv[])
 		}
 		exit(WEXITSTATUS(status));
 	} else {
-		fprintf(stderr, "unknown subcommand %s", argv[1]);
+		fprintf(stderr, "unknown subcommand %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 }
