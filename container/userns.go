68e6f42197cc0a74aac5acc0f06ed5531bfa722b
Scott Moser <scmoser@cisco.com>
2022-11-30 10:23:19 -0800

    feat: Switch MaybeRunInUserns to MaybeRunInNamespace, confine root.
    2 things here:
     * Add functionality to call lxc-wrapper 'usernsnexec' or 'nsexec'
       based upon whether or not the caller is uid 0.
     * Track whether or not we have entered a namespace
       by referencing/adding STACKER_REAL_UID environment variable.
       If that variable is found, then MayberunInNamespace will
       simply execute the command without changing a namespace.
    
    'stacker internal-go cp' is used internally by 'stacker grab'.
    It is run inside a container. Previosly it avoided being put in
    a namespace simply because the Geteuid() was 0.  But now we put
    root in a namespace also, so that won't work.  The change here
    (cmd/main.go) is to make 'stacker internal-go cp' be identified as
    something that should not be run in a namespace.
    
    Last, change the name MayberunInUserns to MaybeRunInNamespace
    to reflect the fact that it won't sometimes it runs in
    a namespace that is not a usernamespace.
    
    Signed-off-by: Scott Moser <scmoser@cisco.com>

--- container/userns.go
+++ container/userns.go
@@ -7,73 +7,84 @@ import (
 
 	"github.com/pkg/errors"
 	stackeridmap "stackerbuild.io/stacker/container/idmap"
-	"stackerbuild.io/stacker/embed-exec"
+	embed_exec "stackerbuild.io/stacker/embed-exec"
 	"stackerbuild.io/stacker/log"
 	"stackerbuild.io/stacker/types"
 )
 
-// A wrapper which runs things in a userns if we're an unprivileged user with
-// an idmap, or runs things on the host if we're root and don't.
-func MaybeRunInUserns(config types.StackerConfig, userCmd []string) error {
-	// TODO: we should try to use user namespaces when we're root as well.
-	// For now we don't.
-	if os.Geteuid() == 0 {
-		log.Debugf("No uid mappings, running as root")
+// A wrapper which executes userCmd in a namespace if stacker has not already
+// entered a namespace.  If it has (STACKER_REAL_UID is set), then it will simply
+// exeucte the userCmd.
+//
+// If the real uid is 0, then use 'nsexec', otherwise use 'usernsexec'.
+func MaybeRunInNamespace(config types.StackerConfig, userCmd []string) error {
+	const envName = "STACKER_REAL_UID"
+	env := os.Environ()
+	realuid := os.Getenv(envName)
+	if realuid != "" {
 		cmd := exec.Command(userCmd[0], userCmd[1:]...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
+		cmd.Env = env
 		return errors.WithStack(cmd.Run())
 	}
 
-	idmapSet, err := stackeridmap.ResolveCurrentIdmapSet()
-	if err != nil {
-		return err
-	}
+	euid := os.Geteuid()
+	env = append(env, fmt.Sprintf("%s=%d", envName, euid))
 
-	if idmapSet == nil {
-		return errors.Errorf("no idmap and not root, can't run %v", userCmd)
-
-	}
-
-	args := []string{"usernsexec"}
-
-	wroteU := false
-	for _, idm := range idmapSet.Idmap {
-		if idm.Isuid {
-			if !wroteU {
-				wroteU = true
-				args = append(args, "u")
-			}
-			args = append(
-				args,
-				fmt.Sprintf("%d", idm.Nsid),
-				fmt.Sprintf("%d", idm.Hostid),
-				fmt.Sprintf("%d", idm.Maprange),
-			)
+	args := []string{}
+	if euid == 0 {
+		args = append(args, "nsexec")
+	} else {
+		idmapSet, err := stackeridmap.ResolveCurrentIdmapSet()
+		if err != nil {
+			return err
 		}
-	}
 
-	wroteG := false
-	for _, idm := range idmapSet.Idmap {
-		if idm.Isgid {
-			if !wroteG {
-				wroteG = true
-				args = append(args, "g")
-			}
-			args = append(
-				args,
-				fmt.Sprintf("%d", idm.Nsid),
-				fmt.Sprintf("%d", idm.Hostid),
-				fmt.Sprintf("%d", idm.Maprange),
-			)
+		if idmapSet == nil {
+			return errors.Errorf("no idmap and not root, can't run %v", userCmd)
 		}
+
+		args = append(args, "usernsexec")
+		wroteU := false
+		for _, idm := range idmapSet.Idmap {
+			if idm.Isuid {
+				if !wroteU {
+					wroteU = true
+					args = append(args, "u")
+				}
+				args = append(
+					args,
+					fmt.Sprintf("%d", idm.Nsid),
+					fmt.Sprintf("%d", idm.Hostid),
+					fmt.Sprintf("%d", idm.Maprange),
+				)
+			}
+		}
+
+		wroteG := false
+		for _, idm := range idmapSet.Idmap {
+			if idm.Isgid {
+				if !wroteG {
+					wroteG = true
+					args = append(args, "g")
+				}
+				args = append(
+					args,
+					fmt.Sprintf("%d", idm.Nsid),
+					fmt.Sprintf("%d", idm.Hostid),
+					fmt.Sprintf("%d", idm.Maprange),
+				)
+			}
+		}
+
 	}
 
 	args = append(args, "--")
 	args = append(args, userCmd...)
 
-	log.Debugf("usernsexec-ing %v", args)
+	log.Debugf("%s-ing %v", args[0], args[1:])
 	cmd, cleanup, err := embed_exec.GetCommand(
 		config.EmbeddedFS,
 		"lxc-wrapper/lxc-wrapper",
@@ -87,5 +98,6 @@ func MaybeRunInUserns(config types.StackerConfig, userCmd []string) error {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = env
 	return errors.WithStack(cmd.Run())
 }
