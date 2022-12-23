4ce73409da023f3d011095c6b9961d7c2e785d76
Ramkumar Chinchani <rchincha@cisco.com>
2022-12-23 23:10:02 +0000

    feat: add a single working-dir option
    Fixes issue #377
    
    Signed-off-by: Ramkumar Chinchani <rchincha@cisco.com>

--- pkg/types/config.go
+++ pkg/types/config.go
@@ -9,6 +9,7 @@ import (
 // StackerConfig is a struct that contains global (or widely used) stacker
 // config options.
 type StackerConfig struct {
+	WorkDir     string `yaml:"work_dir,omitempty"`
 	StackerDir  string `yaml:"stacker_dir"`
 	OCIDir      string `yaml:"oci_dir"`
 	RootFSDir   string `yaml:"rootfs_dir"`
@@ -27,6 +28,7 @@ func (sc *StackerConfig) Substitutions() []string {
 		fmt.Sprintf("STACKER_ROOTFS_DIR=%s", sc.RootFSDir),
 		fmt.Sprintf("STACKER_STACKER_DIR=%s", sc.StackerDir),
 		fmt.Sprintf("STACKER_OCI_DIR=%s", sc.OCIDir),
+		fmt.Sprintf("STACKER_WORK_DIR=%s", sc.WorkDir),
 	}
 }
 
