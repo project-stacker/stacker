bb081ba800af37304e8c700179bca8d5c0f698a6
Ramkumar Chinchani <rchincha@cisco.com>
2023-02-14 17:34:42 +0000

    test: add more UTs
    Signed-off-by: Ramkumar Chinchani <rchincha@cisco.com>

---
+++ pkg/lib/file_test.go
@@ -0,0 +1,53 @@
+package lib_test
+
+import (
+	"io/fs"
+	"os"
+	"path"
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"stackerbuild.io/stacker/pkg/lib"
+)
+
+func TestFile(t *testing.T) {
+	Convey("FileCopy", t, func() {
+		src, err := os.CreateTemp("", "src")
+		So(err, ShouldBeNil)
+		So(src, ShouldNotBeNil)
+		defer os.Remove(src.Name())
+
+		_, err = src.Write([]byte("hello world!"))
+		So(err, ShouldBeNil)
+
+		Convey("With defaults", func() {
+			dest, err := os.CreateTemp("", "dest")
+			So(err, ShouldBeNil)
+			defer os.Remove(dest.Name())
+
+			err = lib.FileCopy(dest.Name(), src.Name(), nil, -1, -1)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("With non-default mode", func() {
+			dest, err := os.CreateTemp("", "dest")
+			So(err, ShouldBeNil)
+			defer os.Remove(dest.Name())
+
+			mode := fs.FileMode(0644)
+			err = lib.FileCopy(dest.Name(), src.Name(), &mode, -1, -1)
+			So(err, ShouldBeNil)
+		})
+	})
+
+	Convey("FindFiles", t, func() {
+		src, err := os.CreateTemp("", "src")
+		So(err, ShouldBeNil)
+		So(src, ShouldNotBeNil)
+		defer os.Remove(src.Name())
+
+		files, err := lib.FindFiles(path.Dir(src.Name()), ".*")
+		So(err, ShouldBeNil)
+		So(files, ShouldNotBeEmpty)
+	})
+}
