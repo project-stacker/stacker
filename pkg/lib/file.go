300f4e5b7696f6daddb99ca89361cf16a310d19a
Scott Moser <smoser@brickies.net>
2023-09-20 10:02:14 -0700

    chore: Add constants UidEmpty and GidEmpty and FileCopyNoPerms function
    Instead of using '-1' to indicate "do not change userid" (or groupid)
    to FileCopy, provide constants UidEmpty and GidEmpty.
    
    Also add simplified FileCopyNoPerms to call FileCopy without mode
    or uid/gid.
    
    Signed-off-by: Scott Moser <smoser@brickies.net>

--- pkg/lib/file.go
+++ pkg/lib/file.go
@@ -10,6 +10,8 @@ import (
 	"github.com/pkg/errors"
 )
 
+const GidEmpty, UidEmpty = -1, -1
+
 func FileCopy(dest string, source string, mode *fs.FileMode, uid, gid int) error {
 	os.RemoveAll(dest)
 
@@ -69,6 +71,10 @@ func FileCopy(dest string, source string, mode *fs.FileMode, uid, gid int) error
 
 	_, err = io.Copy(d, s)
 	return err
+}
+
+func FileCopyNoPerms(dest string, source string) error {
+	return FileCopy(dest, source, nil, UidEmpty, GidEmpty)
 }
 
 // FindFiles searches for paths matching a particular regex under a given folder
