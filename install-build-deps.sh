cff51a7816e0461e4e6dc1544f6c01721565c66d
Ryan Harper <rharper@woxford.com>
2023-10-17 11:23:52 -0500

    feat: Add script to install build requirements for Ubuntu
    doc/install.md is quite out-of-date and does not align with
    what's happening in .github/workflows/build.yml
    
    Changes
      - Add build-essential (for make and friends)
      - Add skopeo/umoci which are needed during build and test
      - Use Ubuntu's golang PPA (instead of snap); Jammy's golang-go is
        1.18 and current go.mod requires 1.20
      - Add in the go module prep and docker clone
    
    Now, install.md says to just run:
    
    sudo ./install-build-deps.sh
    make stacker
    
    Note: the Fedora steps are equally out of date; that'll need another
    PR.
    
    Signed-off-by: Ryan Harper <rharper@woxford.com>

---
+++ install-build-deps.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -o pipefail
+set -o errexit
+
+installdeps_fedora() {
+        sudo dnf install \
+        bats \
+        jq \
+        lxc-devel \
+        libcap-devel \
+        libacl-devel
+    if ! command -v go 2>/dev/null; then
+        sudo dnf install golang
+        go version
+    fi
+}
+
+installdeps_ubuntu() {
+    sudo add-apt-repository -y ppa:project-machine/squashfuse
+    sudo apt -yy install \
+            bats \
+            build-essential \
+            cryptsetup-bin \
+            jq \
+            libacl1-dev \
+            libcap-dev \
+            libcryptsetup-dev \
+            libdevmapper-dev \
+            libpam0g-dev \
+            libseccomp-dev \
+            libselinux1-dev \
+            libssl-dev \
+            libzstd-dev \
+            lxc-dev \
+            lxc-utils \
+            parallel \
+            pkg-config \
+            skopeo \
+            squashfs-tools \
+            squashfuse
+    if ! command -v go 2>/dev/null; then
+        sudo apt -yy install golang-go
+        go version
+    fi
+}
+
+installdeps_golang() {
+    go version
+    GO111MODULE=off go get github.com/opencontainers/umoci/cmd/umoci
+    make download-tools
+    make docker-clone
+    make go-download
+}
+
+. /etc/os-release
+
+# install platform deps
+case $ID_LIKE in
+    debian|ubuntu) installdeps_ubuntu;;
+    redhat|fedora) installdeps_fedora;;
+    *)
+        echo "Unknown os ID_LIKE value $ID_LIKE"
+        exit 1
+        ;;
+esac
+
+# install golang deps
+installdeps_golang || exit 1
